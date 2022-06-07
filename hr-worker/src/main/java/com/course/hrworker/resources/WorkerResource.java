cac4d598577ef2251d2b8f44f53b9a9c4e6117ac
Gabriel Scherer <gabrielscherer2709@gmail.com>
2022-06-06 23:30:34 -0300

    hr-worker implementation

---
+++ hr-worker/src/main/java/com/course/hrworker/resources/WorkerResource.java
@@ -0,0 +1,30 @@
+package com.course.hrworker.resources;
+
+import com.course.hrworker.entities.Worker;
+import com.course.hrworker.repositories.WorkerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/workers")
+public class WorkerResource {
+
+    @Autowired
+    private WorkerRepository repository;
+
+    @GetMapping
+    public ResponseEntity<List<Worker>> findAll(){
+        List<Worker> list = repository.findAll();
+        return ResponseEntity.ok(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Worker> findById(@PathVariable Long id){
+        Worker worker = repository.findById(id).get();
+        return ResponseEntity.ok(worker);
+    }
+}
