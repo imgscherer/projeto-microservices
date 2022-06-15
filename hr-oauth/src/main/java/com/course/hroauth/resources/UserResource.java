42f3759855d2d4bd15765a15647923d7d1458f86
Gabriel Scherer <gabrielscherer2709@gmail.com>
2022-06-14 22:17:09 -0300

    UserFeignClient

--- hr-oauth/src/main/java/com/course/hroauth/resources/UserResource.java
+++ hr-oauth/src/main/java/com/course/hroauth/resources/UserResource.java
@@ -1,27 +1,23 @@
-package com.course.hruser.resources;
+package com.course.hroauth.resources;
 
-import com.course.hruser.entities.User;
-import com.course.hruser.services.UserService;
+import com.course.hroauth.entities.User;
+import com.course.hroauth.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
-
     @Autowired
     private UserService service;
 
-    @GetMapping(value = "/{id}")
-    public ResponseEntity<User> findById(@PathVariable Long id){
-        User user = service.findById(id);
-        return ResponseEntity.ok().body(user);
-    }
-
     @GetMapping(value = "/search")
-    public ResponseEntity<User> findByEmail(@RequestParam String email){
+    public ResponseEntity<User> findByEmail(@RequestParam String email) {
         try {
             User user = service.findByEmail(email);
             return ResponseEntity.ok(user);
--- hr-user/src/main/java/com/course/hruser/services/UserService.java
+++ hr-user/src/main/java/com/course/hruser/services/UserService.java
@@ -3,6 +3,8 @@ package com.course.hruser.services;
 import com.course.hruser.entities.User;
 import com.course.hruser.repositories.UserRepository;
 import com.course.hruser.services.exceptions.ResourceNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,6 +12,8 @@ import java.util.Optional;
 
 @Service
 public class UserService {
+
+    private static Logger logger = LoggerFactory.getLogger(UserService.class);
 
     @Autowired
     private UserRepository repository;
@@ -21,6 +25,11 @@ public class UserService {
 
     public User findByEmail(String email){
         User user = repository.findByEmail(email);
+        if (user == null) {
+            logger.error("Email not found: " + email);
+            throw new IllegalArgumentException("Email not found");
+        }
+        logger.info("Email found: " + email);
         return user;
     }
 }
